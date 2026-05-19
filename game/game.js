a0c12fbe38197bddc5238c456cfa1e6577adf5ad
djbourque <djbourque2@dons.usfca.edu>
2026-05-18 18:08:16 -0700

    finished bulk of js

--- game/game.js
+++ game/game.js
@@ -1,4 +1,4 @@
-//matter aliasing
+//matter.js aliasing
 var Engine = Matter.Engine,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
@@ -17,11 +17,11 @@ var engine = Engine.create();
 var runner = Runner.create();
 Runner.run(runner,engine);
 
-//univeral alliasing
+//map dimensions ------------------------------------------------
 var xbound = 750,
 ybound = 750;
 
-//player parameters
+//player parameters ---------------------------------------------
 var game_player;
 var pWidth = 60;
 var pHeight = 60;
@@ -29,58 +29,42 @@ var pJump_Value = 23;
 var pSpawn_x = 200;
 var pSpawn_y = 650;
 
-//collectable parameters
+//collectable parameters ----------------------------------------
 var cWidth = 60;
 var cHeight = 60;
 var cSpawn_x = 100;
 var cSpawn_y = 100;
 
+//lists of objects
 var game_phys_objects = [];
 var ground_collision_list = [];
 
-var background_img;
+// player's sprite
 var player_sprite;
+
+//map visuals
+var background_img;
 var cloud_platform1;
 var cloud_platform2;
 var cloud_platform3;
 
+//collectible sprites
 var cSprite1;
 var cSprite2;
 var cSprite3;
 
+// constrait objects
 var spring1;
 var spring2;
 var spring3;
 
-/*tests
-class Box {
-    constructor(x, y, w, h){
-    this.body = Bodies.rectangle(x, y, w, h);
-    this.w = w;
-    this.h = h;
-    Composite.add(engine.world, this.body);
-    }
-    show(){
-        var pos = this.body.position;
-        var angle = this.body.angle;
-
-        push();
-        translate(pos.x, pos.y);
-        rotate(angle);
-        rect(0, 0, this.w, this.h);
-        pop();
-    }
-
-}
-
-function mouseDragged() {
-    game_phys_objects.push(new Box(mouseX, mouseY, 20,20));
-}*/
 
 //img loading
 function preload(){
     player_sprite = loadImage('../img/interactive_game_assets_player.png');
+
     background_img = loadImage('../img/interactive_game_assets_background.png');
+
     cloud_platform1 = loadImage('../img/interactive_game_assets_cloud1.png');
     cloud_platform2 = loadImage('../img/interactive_game_assets_cloud2.png');
     cloud_platform3 = loadImage('../img/interactive_game_assets_cloud3.png');
@@ -90,15 +74,20 @@ function preload(){
     cSprite3 = loadImage('../img/interactive_game_assets_collectible3.png');
 }
 
+/*----------------------------------
+Box that is controlled by the player
+------------------------------------
+-----Can jump and move around-------
+----------------------------------*/
 class Player {
     constructor(xSpawn, ySpawn){
-        this.body = Bodies.rectangle(xSpawn, ySpawn, pWidth, pHeight, {inertia: Infinity});
+        this.body = Bodies.rectangle(xSpawn, ySpawn, pWidth, pHeight, {inertia: Infinity}); //inertia is infinity to stop it from rotating
         this.body.friction = 0;
         Composite.add(engine.world, this.body);
         this.jump_time = 0;
         this.img = player_sprite;
     }
-    movement(){
+    movement(){ //function that handles player inputs
         let pos = this.body.position;
         for (let i = 0; i < ground_collision_list.length; i++) {
             if (Collision.collides(this.body, ground_collision_list[i]) != null){
@@ -135,7 +124,7 @@ class Player {
         
         this.jump_time -= 1;
     }
-    show(){
+    show(){// uses p5 to handle rendering
         this.movement();
         let pos = this.body.position;
         let angle = this.body.angle;
@@ -143,20 +132,15 @@ class Player {
         translate(pos.x, pos.y);
         rotate(angle);
 
-        //debug
-        /*
-        if (this.jump_time >= -1){
-            fill(color(255,0,0));
-        }
-        */
-
-        //rect(0, 0, pWidth, pHeight);
         imageMode(CENTER);
         image(this.img, 0, 0, pWidth, pHeight);
         pop();
     }
 }
 
+/*---------------------------------------------------
+Floating objects that attach to and follow the player
+---------------------------------------------------*/
 class Collectible {
     constructor(xSpawn, ySpawn, img) {
         this.xSpawn = xSpawn;
@@ -190,6 +174,10 @@ class Collectible {
     }
 }
 
+
+/*-----------------------------------------
+Invisible objects that act like chain links
+-----------------------------------------*/
 class Link {
     constructor(bodyA, bodyB, isActive = false){
         this.bodyA = bodyA;
@@ -200,27 +188,45 @@ class Link {
 
         this.constraint;
 
-        Events.on(engine, "beforeUpdate", ()=>{//neutralizes force on bodyA
+        // Instead of directly attaching the second object (referred to as "body" in matter.js), I attached one of them to a point that follows the first. 
+        // This neutralizes the tug on the first object, which would interfere with the controls of the player character otherwise.
+
+        if (this.isActive){
+            this.constraint = Constraint.create({
+                pointA: this.pointA,
+                bodyB: this.bodyB,
+                length: 60,
+                stiffness: this.stiffness
+            });
+            Composite.add(engine.world, this.constraint);
+        }
+
+        Events.on(engine, "beforeUpdate", ()=>{
+            if ((Collision.collides(this.bodyA, this.bodyB) != null) && (!this.isActive)){
+                this.constraint = Constraint.create({
+                    pointA: this.pointA,
+                    bodyB: this.bodyB,
+                    length: 120,
+                    stiffness: this.stiffness
+                });
+                Composite.add(engine.world, this.constraint);
+                
+                this.isActive = true;
+            }
+
             if (this.isActive) {
                 this.pointA = bodyA.position;
             }
         });
     }
-    check(){
-        if (!this.isActive){
-            this.constraint = Constraint.create({
-                pointA: this.pointA,
-                bodyB: this.bodyB,
-                length: 120,
-                stiffness: this.stiffness
-            });
-            Composite.add(engine.world, this.constraint);
-            this.isActive = true;
-            console.log(this.constraint);
-        }
-    }
 }
 
+/*-----------------------------------------------------
+-Objects that restore the player's jump when landed on.
+-------------------------------------------------------
+Actually made of 2 boxes, one to function as a surface, 
+the other to restore the player's jump when in contact.
+-----------------------------------------------------*/
 class Platform {
     constructor(img, x, y, w, h = 60){
         this.img = img;
@@ -262,7 +268,9 @@ function setup() {
 
     game_player = new Player(pSpawn_x, pSpawn_y);
     collectable1 = new Collectible(cSpawn_x, cSpawn_y, cSprite1);
+    //collectable2 = new Collectible(cSpawn_x+120, cSpawn_y, cSprite2);
     spring1 = new Link(game_player.body, collectable1.body);
+    //spring2 = new Link(collectable1.body, collectable2.body);
 
 
     // BUILDS THE MAP GEOMETRY, Main difference between each page
@@ -287,7 +295,5 @@ function draw(){
     }
     game_player.show();
     collectable1.show();
-    if (Collision.collides(game_player.body, collectable1.body) != null){
-        spring1.check();
-    }
+    //collectable2.show();
 }
